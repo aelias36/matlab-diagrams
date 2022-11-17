efdb0c2bd6213eee0b0a7a4bb6f4ab2d3d8a54a5
Alex Elias <aelias36@yahoo.com>
2022-11-17 14:35:30 -0500

    Use 3D line for arrow for proper draw order

--- +diagrams/arrow.m
+++ +diagrams/arrow.m
@@ -1,6 +1,7 @@
 classdef arrow < handle
     properties
         an
+        h_line
         tail
         head
     end
@@ -19,23 +20,48 @@ classdef arrow < handle
 
             plot3(tail(1),tail(2),tail(3)); % zoom figure out enough
             plot3(head(1),head(2),head(3));
+            obj.h_line = diagrams.line(tail, head, ...
+                LineStyle=parser.Results.LineStyle, ...
+                LineWidth=parser.Results.LineWidth, ...
+                Color=parser.Results.Color);
 
             obj.tail=tail; obj.head=head;
             [x_tail, y_tail] = diagrams.utils.ds2fig.ds2fig(tail(1), tail(2), tail(3));
             [x_head, y_head] = diagrams.utils.ds2fig.ds2fig(head(1), head(2), head(3));
             obj.an = annotation('doublearrow', ...
                 [x_tail x_head], [y_tail y_head], ...
-                LineStyle=parser.Results.LineStyle, ...
+                LineStyle='none',...%parser.Results.LineStyle, ...
                 Head2Style=parser.Results.HeadStyle, ...
                 Head1Style=parser.Results.TailStyle, ...
                 Color=parser.Results.Color, ...
                  LineWidth=parser.Results.LineWidth);
+
         end
         
         function update(obj)
             [x_tail, y_tail] = diagrams.utils.ds2fig.ds2fig(obj.tail(1), obj.tail(2), obj.tail(3));
             [x_head, y_head] = diagrams.utils.ds2fig.ds2fig(obj.head(1), obj.head(2), obj.head(3));
             set(obj.an,'X',[x_tail x_head],'Y',[y_tail y_head]);
+            
+            
+            % Move line to avoid overlap with arrowhead
+            set(obj.an, 'Units', 'points');
+            current_len = norm(obj.an.Position(3:4));
+            set(obj.an, 'Units', 'normalized');
+
+            if obj.an.Head2Style ~= "none"
+                new_line_head = obj.tail+(obj.head-obj.tail)*(current_len-7)/current_len;
+            else
+                new_line_head = obj.head;
+            end
+            
+            if obj.an.Head1Style ~= "none"
+                new_line_tail = obj.head+(obj.tail-obj.head)*(current_len-7)/current_len;
+            else
+                new_line_tail = obj.tail;
+            end
+
+                diagrams.utils.plot3_mat_update(obj.h_line, [new_line_tail, new_line_head]);
         end
 
 
