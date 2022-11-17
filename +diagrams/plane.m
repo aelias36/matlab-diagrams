193002f23877b2bdd813bea41a08323b71c0b1f7
Alex Elias <aelias36@yahoo.com>
2022-11-17 14:34:50 -0500

    Add to `plane`: option for 4 different lengths

--- +diagrams/plane.m
+++ +diagrams/plane.m
@@ -1,10 +1,14 @@
 function plane(center, perp, edge, radius)
+if numel(radius)==1
+    radius = radius*[1 1 1 1];
+end
+
 perp = perp / norm(perp);
 edge = edge / norm(edge);
 
 edge_2 = cross(perp, edge);
 
-pts = center + radius*[edge+edge_2 edge-edge_2 -edge-edge_2 -edge+edge_2 edge+edge_2];
+pts = center + [radius(1)*edge+radius(2)*edge_2 radius(1)*edge-radius(4)*edge_2 -radius(3)*edge-radius(4)*edge_2 -radius(3)*edge+radius(2)*edge_2 radius(1)*edge+radius(2)*edge_2];
 
 plot3(pts(1,:), pts(2,:), pts(3,:), 'k')
 end
