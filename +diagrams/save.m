45e694a24018e55c57a5cd3894a1aac69540abe0
Alex Elias <aelias36@yahoo.com>
2022-12-01 11:05:48 -0500

    Make arguments in `save()` optional

--- +diagrams/save.m
+++ +diagrams/save.m
@@ -1,4 +1,16 @@
 function save(h_fig, name, location)
+    if nargin < 1
+        h_fig = gcf;
+    end
+
+    if nargin < 2
+        [~, name, ~] = fileparts(matlab.desktop.editor.getActiveFilename);
+    end
+
+    if nargin < 3
+        location = "output";
+    end
+
     prev = cd(location);
     saveas(h_fig, name, 'svg');
     saveas(h_fig, name, 'epsc'); % epsc for color
