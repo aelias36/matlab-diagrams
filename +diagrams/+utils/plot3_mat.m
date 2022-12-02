d81734ed832b582c6b973110aabbc3b87554afb9
Alex Elias <aelias36@yahoo.com>
2022-12-02 15:14:26 -0500

    Add optional args to `plot3_mat()`

--- +diagrams/+utils/plot3_mat.m
+++ +diagrams/+utils/plot3_mat.m
@@ -1,3 +1,3 @@
-function h = plot3_mat(mat)
-    h = plot3(mat(1,:),mat(2,:),mat(3,:), 'k', 'LineWidth',1.5);
+function h = plot3_mat(mat, varargin)
+    h = plot3(mat(1,:),mat(2,:),mat(3,:), 'k', 'LineWidth',1.5, varargin{:});
 end
