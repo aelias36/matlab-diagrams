1526edc9c78924f8038e050b0779e3b9a5ed6fcb
Alex Elias <aelias36@yahoo.com>
2022-12-20 14:32:49 -0500

    Add optional arguments to `plane()`

--- +diagrams/plane.m
+++ +diagrams/plane.m
@@ -1,4 +1,4 @@
-function plane(center, perp, edge, radius)
+function plane(center, perp, edge, radius, varargin)
 if numel(radius)==1
     radius = radius*[1 1 1 1];
 end
@@ -10,5 +10,5 @@ edge_2 = cross(perp, edge);
 
 pts = center + [radius(1)*edge+radius(2)*edge_2 radius(1)*edge-radius(4)*edge_2 -radius(3)*edge-radius(4)*edge_2 -radius(3)*edge+radius(2)*edge_2 radius(1)*edge+radius(2)*edge_2];
 
-plot3(pts(1,:), pts(2,:), pts(3,:), 'k')
+plot3(pts(1,:), pts(2,:), pts(3,:), 'k', varargin{:})
 end
