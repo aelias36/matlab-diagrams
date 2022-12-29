6ff5000777a454d1544a6eb756fed103741224c0
Alex Elias <aelias36@yahoo.com>
2022-12-29 15:31:59 -0500

    Optional arguments for `circle()` (and make green slightly darker)

--- +diagrams/circle.m
+++ +diagrams/circle.m
@@ -1,7 +1,7 @@
-function h = circle(center, axis, radius)
+function h = circle(center, axis, radius, varargin)
 p = radius * diagrams.utils.perp_vector(axis);
 C = center + diagrams.utils.gen_circle(axis, p);
 
-h = diagrams.utils.plot3_mat(C);
+h = diagrams.utils.plot3_mat(C, varargin{:});
 
 end
--- +diagrams/colors.m
+++ +diagrams/colors.m
@@ -2,7 +2,7 @@ classdef colors
     properties (Constant)
         red  = [0.9 0.1 0.1];
         blue = [0.1 0.1 0.9];
-        green = [0.1 0.7 0.1];
+        green = [0.1 0.6 0.1];
     end
 end
 
