7a050e67a51536dcb9bff3d0bb2cd5ea5191ad97
Alex Elias <aelias36@yahoo.com>
2024-12-17 18:55:43 -0500

    Update circle.m

--- +diagrams/circle.m
+++ +diagrams/circle.m
@@ -1,7 +1,6 @@
 function h = circle(center, axis, radius, varargin)
-p = radius * diagrams.utils.perp_vector(axis);
-C = center + diagrams.utils.gen_circle(axis, p);
-
-h = diagrams.utils.plot3_mat(C, varargin{:});
-
+    p = radius * diagrams.utils.perp_vector(axis);
+    C = center + diagrams.utils.gen_circle(axis, p);
+    
+    h = diagrams.utils.plot3_mat(C, varargin{:});
 end
