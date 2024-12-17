06cc091da635816090d54c63763b3b29e7a10e77
Alex Elias <aelias36@yahoo.com>
2024-12-17 18:48:38 -0500

    Update dot.m

--- +diagrams/dot.m
+++ +diagrams/dot.m
@@ -6,28 +6,31 @@ classdef dot < handle
     end
     
     methods
-        function obj = dot(loc, varargin)
-            diagrams.utils.register_update(obj)
-
-            parser = inputParser;
-            addParameter(parser,'Color', [0 0 0]);
-            addParameter(parser, 'MarkerSize', 3)
-            addParameter(parser, 'Is2D', false)
-            parse(parser,varargin{:});
-
-            obj.size = parser.Results.MarkerSize;
-            
-            if parser.Results.Is2D
+        function obj = dot(loc, opts)
+            arguments
+                loc (3,1) double
+                opts.Color (1,3) double = [0 0 0]
+                opts.MarkerSize (1,1) double = 3
+                opts.Is2D (1,1) logical = false
+            end
+        
+            diagrams.utils.register_update(obj);
+        
+            obj.size = opts.MarkerSize;
+        
+            if opts.Is2D
                 plot(loc(1), loc(2));
             else
-                plot3(loc(1),loc(2),loc(3)); % zoom figure out enough
+                plot3(loc(1), loc(2), loc(3)); % zoom figure out enough
             end
-            
-            obj.loc=loc;
-            obj.an = annotation('ellipse', 'Color',parser.Results.Color, 'FaceColor',parser.Results.Color);
+        
+            obj.loc = loc;
+            obj.an = annotation('ellipse', ...
+                                 'Color', opts.Color, ...
+                                 'FaceColor', opts.Color);
             obj.update();
-            
         end
+
         
         function update(obj)
             [x, y] = diagrams.utils.ds2fig.ds2fig(obj.loc(1),obj.loc(2),obj.loc(3));
