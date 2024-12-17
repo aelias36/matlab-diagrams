d0bd6f40c1760229e89b1163ebcc20d27b9c00fe
Alex Elias <aelias36@yahoo.com>
2024-12-17 18:59:56 -0500

    Update plane.m

--- +diagrams/plane.m
+++ +diagrams/plane.m
@@ -1,14 +1,14 @@
 function plane(center, perp, edge, radius, varargin)
-if isscalar(radius)
-    radius = radius*[1 1 1 1];
-end
-
-perp = perp / norm(perp);
-edge = edge / norm(edge);
-
-edge_2 = cross(perp, edge);
-
-pts = center + [radius(1)*edge+radius(2)*edge_2 radius(1)*edge-radius(4)*edge_2 -radius(3)*edge-radius(4)*edge_2 -radius(3)*edge+radius(2)*edge_2 radius(1)*edge+radius(2)*edge_2];
-
-plot3(pts(1,:), pts(2,:), pts(3,:), 'k', varargin{:})
+    if isscalar(radius)
+        radius = radius*[1 1 1 1];
+    end
+    
+    perp = perp / norm(perp);
+    edge = edge / norm(edge);
+    
+    edge_2 = cross(perp, edge);
+    
+    pts = center + [radius(1)*edge+radius(2)*edge_2 radius(1)*edge-radius(4)*edge_2 -radius(3)*edge-radius(4)*edge_2 -radius(3)*edge+radius(2)*edge_2 radius(1)*edge+radius(2)*edge_2];
+    
+    plot3(pts(1,:), pts(2,:), pts(3,:), 'k', varargin{:})
 end
