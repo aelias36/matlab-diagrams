b77271094f3e930aa9252ce8d3e3a36bfae7a537
Alex Elias <aelias36@yahoo.com>
2024-12-17 19:05:31 -0500

    Update rot.m

--- +diagrams/rot.m
+++ +diagrams/rot.m
@@ -1,6 +1,6 @@
 function R=rot(k,theta)
     k = k / norm(k);
-    R = eye(3,3)+sin(theta)*hat(k)+(1-cos(theta))*hat(k)*hat(k);
+    R = eye(3,3) + sin(theta)*hat(k) + (1-cos(theta))*hat(k)*hat(k);
 end  
 
 function khat = hat(k)
