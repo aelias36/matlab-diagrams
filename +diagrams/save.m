03a7d923febf95fbfe8f8cad0c1e9345574e7998
Alex Elias <aelias36@yahoo.com>
2025-04-29 16:49:50 -0400

    Only save `.eps` by default

--- +diagrams/save.m
+++ +diagrams/save.m
@@ -12,7 +12,7 @@ function save(h_fig, name, location)
     end
 
     prev = cd(location);
-    saveas(h_fig, name, 'svg');
+    % saveas(h_fig, name, 'svg');
     saveas(h_fig, name, 'epsc'); % epsc for color
     %saveas(h_fig, name, 'pdf');
     cd(prev);
