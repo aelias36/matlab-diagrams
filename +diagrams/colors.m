ff2e6257969cee67b95dead65f23703e6b2a457e
Alex Elias <aelias36@yahoo.com>
2025-04-29 16:48:58 -0400

    New colors: purple and orange

--- +diagrams/colors.m
+++ +diagrams/colors.m
@@ -5,6 +5,8 @@ classdef colors
         red  = [0.9 0.1 0.1];
         blue = [0.1 0.1 0.9];
         green = [0.1 0.6 0.1];
+        purple = [0.55 0 0.78];
+        orange = [0.75 0.45 0];
         dark_red = [0.6 0.05 0.05];
         dark_green = [0.05 0.3 0.05];
     end
